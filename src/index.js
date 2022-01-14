0a38b10b42791a156bb3b610de1db054dee41b74
Evgeniy652 <evgen652@mail.ru>
2022-01-14 13:34:34 +0300

    feat:implemented the function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let value=Math.abs(n);
+  let nArray= value.toString().split('');
+  let nArray2=nArray.reverse();
+  let nString= nArray2.join('');
+  return +nString;
 }
